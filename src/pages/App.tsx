2b90d48a68ce71e033174f56df456182f8f4bba8
lucasdeister <lucasdeister7@hotmail.com>
2024-10-31 16:24:32 -0300

    Incluído controle de versão

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,8 +6,27 @@ import { ModalProvider } from '../context/ModalContext';
 import { CronometroProvider } from '../context/CronometroContext';
 import { UtilProvider } from '../context/UtilContext';
 import style from "./App.module.css"
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const checkAppVersion = () => {
+      // Obtenha a versão atual do HTML
+      const currentVersion = document.querySelector('meta[name="app-version"]')?.getAttribute('content');
+
+      // Obtenha a última versão salva no localStorage
+      const savedVersion = localStorage.getItem('appVersion');
+
+      // Se a versão mudou, limpe o cache e atualize a página
+      if (currentVersion && currentVersion !== savedVersion) {
+        localStorage.setItem('appVersion', currentVersion);
+        window.location.reload(); // Recarrega ignorando o cache
+      }
+    };
+
+    checkAppVersion();
+  }, []);
   return (
     <div className={style.grid_container}>
       <Header/>
